da3d97f8d446cacca17e8f1db67b81e20bfb4cc1
Noam <noamrose19@gmail.com>
2025-01-17 00:05:20 +0200

    SolvedCell implements ICell and is cell with a fixed value

---
+++ SolvedCell.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sodoku
+{
+    internal class SolvedCell : ICell
+    {
+        public int _value { get; }
+        public int _row { get; }
+        public int _col { get; }
+        public int _box { get; }
+
+        public SolvedCell(int row, int col, int box, int value)
+        {
+            _row = row;
+            _col = col;
+            _box = box;
+            _value = value;
+        }
+    }
+}
