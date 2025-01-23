1202c1d51b874f5a1175f5fe95e01ec115888810
NoamTheGoat <noamrose19@gmail.com>
2025-01-24 00:58:58 +0200

    Handles input, validifies it and turns the input string into and array

---
+++ InputHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Sodoku.GlobalConstants;
+
+namespace Sodoku
+{
+    internal static class InputHandler
+    {
+        public static int[] StringToArray(string input)
+        {
+            int[] result = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                result[i] = input[i] - '0';
+            }
+            return result;
+        }
+        public static bool IsValidInput(string input)
+        {
+            if (input != null && input.Length == BoardLength*BoardLength && input.All(char.IsDigit))
+            {
+                return true; 
+            } 
+            return false;
+        }
+
+        private static bool IsDigitsOnly(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9')
+                    return false;
+            }
+            return true;
+        }
+    }
+}
