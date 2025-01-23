30dacfed84e1c310940618bd14162d7a591c57e3
NoamTheGoat <noamrose19@gmail.com>
2025-01-24 01:07:37 +0200

    Orgenized my project in files and added a main file

---
+++ Cell/SolvedCell.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sodoku
+{
+    internal class SolvedCell : ICell
+    {
+        public int _value { get; }
+        public int _row { get; }
+        public int _col { get; }
+        public int _box { get; }
+
+        public SolvedCell(int row, int col, int box, int value)
+        {
+            _row = row;
+            _col = col;
+            _box = box;
+            _value = value;
+        }
+        public SolvedCell(SolvedCell cell)
+        {
+            _row = cell._row;
+            _col = cell._col;
+            _box = cell._box;
+            _value = cell._value;
+        }
+    }
+}
---
+++ main.cs
@@ -0,0 +1,45 @@
+﻿using Sodoku.CustomExceptions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sodoku
+{
+    internal class main
+    {
+        public static void Main(String[] args)
+        {
+            
+            string input = Console.ReadLine();
+            if (!InputHandler.IsValidInput(input)) 
+            {
+                throw new NotVaildInputException();
+            }
+
+            int[] a = InputHandler.StringToArray(input);
+
+            SodokuSolver solver = new SodokuSolver(a);
+            solver.PrintSodokuBoard();
+            if (!solver.IsValidSodokuBoard())
+            {
+                throw new NotVaildBoardException();
+            }
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            //solver.PrintSodokuBoard();
+            if (solver.SolveSodoku())
+            {
+                solver.PrintSodokuBoard();
+            }
+            else 
+            {
+                Console.WriteLine("not solved");
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"Elapsed time: {stopwatch.Elapsed.TotalSeconds} seconds");
+        }
+    }
+}
