211a0b7ec046bcf5f47e1e2b7ec2863ff3ba1233
NoamTheGoat <noamrose19@gmail.com>
2025-01-27 18:21:41 +0200

    A folder of different heuristics currently has 2, hidden single heuristic and naked pair heuristic

---
+++ Heuristics/HiddenSingleHeuristic.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Sodoku.GlobalConstants;
+
+namespace Sodoku.Heuristics
+{
+    public static class HiddenSingleHeuristic
+    {
+        public static bool HandleHiddenSingles(IBoard board)
+        {
+            bool isChanged = false;
+
+            for (int i = 0; i < BoardLength; i++)
+            {
+                isChanged |= ProcessGroupForNakedSingles(board, board.GetRow(i));
+                isChanged |= ProcessGroupForNakedSingles(board, board.GetCol(i));
+                isChanged |= ProcessGroupForNakedSingles(board, board.GetBox(i + 1));
+            }
+
+            return isChanged;
+        }
+
+
+        private static bool ProcessGroupForNakedSingles(IBoard board, List<UnsolvedCell> unsolvedCells)
+        {
+            bool isChanged = false;
+            var optionDictionary = new Dictionary<int, List<UnsolvedCell>>();
+
+            // Step 1: Map each option to the cells it appears in
+            foreach (var cell in unsolvedCells)
+            {
+                foreach (int option in cell._options)
+                {
+                    if (!optionDictionary.ContainsKey(option))
+                    {
+                        optionDictionary[option] = new List<UnsolvedCell>();
+                    }
+                    optionDictionary[option].Add(cell);
+                }
+            }
+
+            // Step 2: Apply naked singles
+            foreach (var kvp in optionDictionary)
+            {
+                int option = kvp.Key;
+                List<UnsolvedCell> cells = kvp.Value;
+
+                if (cells.Count == 1)
+                {
+                    var cell = cells[0];
+                    if (cell._options.Count != 1 || !cell._options.Contains(option))
+                    {
+                        cell._options.Clear();
+                        cell._options.Add(option);
+                        isChanged = true; // A change has been made
+                    }
+                }
+            }
+
+            return isChanged;
+        }
+    }
+}
---
+++ Heuristics/NakedPairsHeuristic.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Sodoku.GlobalConstants;
+
+namespace Sodoku.Heuristics
+{
+    public static class NakedPairsHeuristic
+    {
+        public static bool FindAndEliminateNakedPairs(IBoard board)
+        {
+            bool isChanged = false;
+            var groupSet = new List<UnsolvedCell>();
+
+            // Collect all unsolved cells with exactly 'pairSize' options
+            for (int i = 0; i < BoardLength; i++)
+            {
+                for (int j = 0; j < BoardLength; j++)
+                {
+                    if (board.GetCellInPosition(i, j) is UnsolvedCell tempCell
+                        && tempCell._options.Count == pairSize)
+                    {
+                        groupSet.Add(tempCell);
+                    }
+                }
+            }
+
+            // Check for naked pairs within the collected group
+            for (int i = 0; i < groupSet.Count; i++)
+            {
+                for (int j = i + 1; j < groupSet.Count; j++)
+                {
+                    if (HasEqualOptions(groupSet[i], groupSet[j]))
+                    {
+                        var nakedSet = new List<UnsolvedCell> { groupSet[i], groupSet[j] };
+                        bool pairChanged = false;
+
+                        if (groupSet[i]._box == groupSet[j]._box)
+                        {
+                            pairChanged = EliminateNakedSets(board, board.GetBox(groupSet[i]._box), nakedSet);
+                        }
+                        else if (groupSet[i]._col == groupSet[j]._col)
+                        {
+                            pairChanged = EliminateNakedSets(board, board.GetCol(groupSet[i]._col), nakedSet);
+                        }
+                        else if (groupSet[i]._row == groupSet[j]._row)
+                        {
+                            pairChanged = EliminateNakedSets(board, board.GetRow(groupSet[i]._row), nakedSet);
+                        }
+
+                        isChanged |= pairChanged; // Track if any change occurred
+                    }
+                }
+            }
+
+            return isChanged;
+        }
+
+
+        private static bool EliminateNakedSets(IBoard board, List<UnsolvedCell> group, List<UnsolvedCell> nakedSet)
+        {
+            bool isChanged = false;
+            HashSet<int> nakedOptions = new HashSet<int>(nakedSet.First()._options);
+
+            foreach (var cell in group)
+            {
+                if (!nakedSet.Contains(cell))
+                {
+                    int initialOptionCount = cell._options.Count;
+                    cell._options.ExceptWith(nakedOptions);
+
+                    if (cell._options.Count != initialOptionCount) // Check if options were modified
+                    {
+                        isChanged = true;
+                    }
+                }
+            }
+
+            return isChanged;
+        }
+        private static bool HasEqualOptions(UnsolvedCell Cell1, UnsolvedCell Cell2)
+        {
+            return Cell1._options.SetEquals(Cell2._options);
+        }
+    }
+}
