20509f8823add5c6d12a8e3b967034315deeb39f
NoamTheGoat <noamrose19@gmail.com>
2025-01-31 21:20:33 +0200

    added new custom exception for the case that the input is empty

---
+++ CustomExceptions/EmptyInputException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sodoku.CustomExceptions
+{
+    public class EmptyInputException : Exception
+    {
+        public EmptyInputException()
+        { }
+        public EmptyInputException(string message)
+        : base(message)
+        { }
+
+        public EmptyInputException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
