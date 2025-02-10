618f08371b01ed95596b189c9cbb61a9e1f978a0
NoamTheGoat <noamrose19@gmail.com>
2025-02-10 11:36:36 +0200

    added the Tests project

---
+++ SodokuTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SodokuTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("WIS")]
+[assembly: AssemblyProduct("SodokuTests")]
+[assembly: AssemblyCopyright("Copyright © WIS 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f4695885-27cf-4108-91ec-134d25a3c328")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
